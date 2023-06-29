de58633c24fa92bc9bfe1ec62f4fa190673f68c9
oysandvik94 <oysandvik94@gmail.com>
2023-06-29 09:29:57 +0200

    First working version of plugin

---
+++ lua/boilercreate/init.lua
@@ -0,0 +1,36 @@
+local function BClass(fileName)
+    local workspacePath = vim.api.nvim_call_function("getcwd", {})
+    local currentBufferPath = vim.api.nvim_call_function("expand", { "%:p:h" })
+
+    local namespace = currentBufferPath:gsub(workspacePath, '')
+    namespace = namespace:gsub("/", ".")
+    namespace = namespace:gsub(fileName, "")
+    namespace = namespace:gsub("^%.", "")
+
+    local filePath = currentBufferPath .. "/" .. fileName
+    local file, err = io.open(filePath, "w")
+
+    if not file then
+        print("Error opening file: " .. err)
+        return
+    end
+
+    file:write(string.format("using %s;\n\n", namespace))
+    file:write(string.format("public class %s\n{\n\n}", fileName))
+    file:close()
+
+    vim.api.nvim_command("e " .. filePath)
+
+    -- Find the opening curly brace using Vim commands
+    vim.api.nvim_command("/\\v[{]")
+
+    -- Adjust the cursor position to be between the braces
+    local cursorLine, cursorCol = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_win_set_cursor(0, { cursorLine + 1, cursorCol + 1 })
+end
+
+vim.api.nvim_create_user_command('BClass',
+    function(opts)
+        BClass(opts.args)
+    end
+    , { nargs = 1 })
